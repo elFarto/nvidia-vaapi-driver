fd2b6f07231da52041e17a7531e1d767a283416e
Stephen <elfarto@elfarto.com>
2022-01-16 09:22:12 +0000

    Added support for AV1 as a build time option.

--- samples/gensamples.sh
+++ samples/gensamples.sh
@@ -9,5 +9,8 @@ ffmpeg -f lavfi -i smptebars=duration=10:size=640x360:rate=30 -c:v libx265 -crf 
 ffmpeg -f lavfi -i smptebars=duration=10:size=640x360:rate=30 -c:v libx265 -crf 26 -preset fast -pix_fmt yuv422p12le smptebars_hevc_422_12bit.mp4
 ffmpeg -f lavfi -i smptebars=duration=10:size=640x360:rate=30 -c:v mpeg4 smptebars_mpeg4.mp4
 ffmpeg -f lavfi -i smptebars=duration=10:size=640x360:rate=30 -c:v vp9 smptebars_vp9.mp4
+#this one didn't work on my ffmpeg, complained about a mismatched ABI
+#ffmpeg -f lavfi -i smptebars=duration=10:size=640x360:rate=30 -c:v av1 smptebars_av1.mp4
+ffmpeg -f lavfi -i smptebars=duration=10:size=640x360:rate=30 -c:v libsvtav1 smptebars_av1.mp4
 
-#TODO need av1, mpeg2, mpeg4, vc1, vp8, perhaps h264 with different levels
+#TODO need mpeg2, vc1, vp8, perhaps h264 with different levels
