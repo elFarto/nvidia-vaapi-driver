f716f65fb47769b63d6103363ee3c6b3eb6b2fbd
Stephen <elfarto@elfarto.com>
2022-07-17 09:50:42 +0100

    more clean up, removed unneeded alignment and page size, and added fix for 470 driver series

--- src/direct/nv-driver.h
+++ src/direct/nv-driver.h
@@ -16,6 +16,8 @@ typedef struct {
     NvHandle clientObject;
     NvHandle deviceObject;
     NvHandle subdeviceObject;
+    uint32_t driverMajorVersion;
+    //bool hasHugePage;
 } NVDriverContext;
 
 typedef struct {
@@ -33,7 +35,7 @@ typedef struct {
 bool init_nvdriver(NVDriverContext *context, int drmFd);
 bool free_nvdriver(NVDriverContext *context);
 bool get_device_uuid(NVDriverContext *context, char uuid[16]);
-bool alloc_memory(NVDriverContext *context, uint32_t size, uint32_t alignment, uint32_t bpc, int *fd);
+bool alloc_memory(NVDriverContext *context, uint32_t size, int *fd);
 bool alloc_image(NVDriverContext *context, uint32_t width, uint32_t height, uint8_t channels, uint8_t bytesPerChannel, NVDriverImage *image);
 
 #endif
