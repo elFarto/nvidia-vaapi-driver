be57f23700517d4978a45bbc92ce195a311b1260
Emil Velikov <emil.l.velikov@gmail.com>
2023-01-31 14:06:40 +0000

    Drop -1 handling in nvidia-drm/modeset helpers
    It's unreachable code path.
    
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

--- src/vabackend.c
+++ src/vabackend.c
@@ -2165,35 +2165,29 @@ static const struct VADriverVTable vtable = {
 };
 
 static bool checkModesetParameterFromFd(int fd) {
-    if (fd > 0) {
-        //this ioctl should fail if modeset=0
-        struct drm_get_cap caps = { .capability = DRM_CAP_DUMB_BUFFER };
-        int ret = ioctl(fd, DRM_IOCTL_GET_CAP, &caps);
-        if (ret != 0) {
-            //the modeset parameter is set to 0
-            LOG("ERROR: This driver requires the nvidia_drm.modeset kernel module parameter set to 1");
-            return false;
-        }
-        return true;
+    //this ioctl should fail if modeset=0
+    struct drm_get_cap caps = { .capability = DRM_CAP_DUMB_BUFFER };
+    int ret = ioctl(fd, DRM_IOCTL_GET_CAP, &caps);
+    if (ret != 0) {
+        //the modeset parameter is set to 0
+        LOG("ERROR: This driver requires the nvidia_drm.modeset kernel module parameter set to 1");
+        return false;
     }
     return true;
 }
 
 static bool isNvidiaDrmFd(int fd) {
-    if (fd > 0) {
-        char name[16] = {0};
-        struct drm_version ver = {
-            .name = name,
-            .name_len = 15
-        };
-        int ret = ioctl(fd, DRM_IOCTL_VERSION, &ver);
-        if (ret || strncmp(name, "nvidia-drm", 10)) {
-            LOG("Invalid driver for DRM device: %s", ver.name);
-            return false;
-        }
-        return true;
+    char name[16] = {0};
+    struct drm_version ver = {
+        .name = name,
+        .name_len = 15
+    };
+    int ret = ioctl(fd, DRM_IOCTL_VERSION, &ver);
+    if (ret || strncmp(name, "nvidia-drm", 10)) {
+        LOG("Invalid driver for DRM device: %s", ver.name);
+        return false;
     }
-    return false;
+    return true;
 }
 
 __attribute__((visibility("default")))
